b56a23cb67c28f4604ef19bd1bab28bb6bf38efd
RtyXmd <rtyxmd753951@gmail.com>
2021-11-16 14:47:27 +0800

    clean

--- vue2/src/views/Home.vue
+++ vue2/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="vue2 index"/>
   </div>
 </template>
 
